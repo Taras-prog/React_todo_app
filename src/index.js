52401600304e5c81112346636580f47be9e5f2c9
Taras-prog <tarascr7@gmail.com>
2020-07-26 12:51:53 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element =(
+    <div>
+        <h1>My Todo Lit</h1>
+        <input placeholder="search"/>
+     <ul>
+        <li>Learn React</li>
+        <li>Learn NodeJS</li>
+    </ul>
+    </div>
+    )
+
+
+ReactDOM.render(element,  document.getElementById('root'))
