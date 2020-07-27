7cbcb38999388faad003eb10d700e68a7a4362b3
Taras-prog <tarascr7@gmail.com>
2020-07-27 14:58:24 +0300

    css commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import TodoList from './components/todo-list';
 
 const App = () => {
     const todoData = [
-        { label: 'Drink Coffee', important: false},
-        { label: 'Learn React', important: true},
-        { label: 'Learn NodeJS', important: true},
-        { label: 'Learn English', important: true},
-        { label: 'Learn Python', important: false},
+        { label: 'Drink Coffee', important: false, id: 1},
+        { label: 'Learn React', important: true, id: 2},
+        { label: 'Learn NodeJS', important: true, id: 3},
+        { label: 'Learn English', important: true, id: 4},
+        { label: 'Learn Python', important: false, id: 5},
     ]
   
     return ( 
